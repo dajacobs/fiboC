204f5e81dde61f1e6db48baf2e0373db3a9ee2cf
dajacobs <dajacobs.tab@gmail.com>
2015-09-08 08:39:02 -0500

    fibonacci recursive with input

--- fibonacci.c
+++ fibonacci.c
@@ -1,9 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
  
 int Fibonacci(int);
  
-main()
-{
+main() {
    int n, i = 1, c;
    
    printf("Enter an input\n");
@@ -11,21 +10,19 @@ main()
  
    printf("Fibonacci series\n");
  
-   for ( c = 1 ; c <= n ; c++ )
-   {
+   for(c = 1 ; c <= n ; c++) {
       printf("%d\n", Fibonacci(i));
       i++; 
    }
- 
    return 0;
 }
  
-int Fibonacci(int n)
-{
-   if ( n == 0 )
-      return 0;
-   else if ( n == 1 )
-      return 1;
-   else
-      return ( Fibonacci(n-1) + Fibonacci(n-2) );
+int Fibonacci(int n) {
+   if(n == 0) {
+   		return 0;
+   } else if (n == 1) {
+   		return 1;
+   } else {
+   		return(Fibonacci(n - 1) + Fibonacci(n - 2));
+   }
 } 
